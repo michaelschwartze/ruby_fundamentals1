24546c98a6f6dcf82fc3bf743dcf7d1fbbefc0ae
Michael Schwartze <mschwartze@gmail.com>
2016-10-18 14:01:06 -0400

    add exercise4

---
+++ exercise4.rb
@@ -0,0 +1,12 @@
+#comment
+(1..100).each do |x|
+  if (x % 3 == 0) && (x % 5 == 0)
+    puts "Bitmaker"
+  elsif x % 5 == 0
+    puts "Maker"
+  elsif x % 3 == 0
+    puts "Bit" 
+  else
+    puts x
+  end
+end
